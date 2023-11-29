84afaed386f40bb57fd0c91d959639e7b65b73ef
sophistiqq <roi.for.school@gmail.com>
2023-11-29 19:00:34 +0800

    register button fixed

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -9,7 +9,13 @@ closeBtn.addEventListener('click', () => {
     popup.classList.remove('active')
 })
 
+const signin = document.querySelector('.signin')
+const register = document.querySelector('.register')
+const signinOrRegister = document.querySelector('#signinOrRegister')
 
-
-
+signinOrRegister.addEventListener('click', () => {
+    register.classList.toggle('hide');
+    signin.classList.toggle('hide');
+    signinOrRegister.innerHTML = signinOrRegister.innerHTML === 'Sign In' ? 'Register' : 'Sign In';
+});
 
